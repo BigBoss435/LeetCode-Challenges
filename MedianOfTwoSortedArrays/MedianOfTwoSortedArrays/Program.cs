a113166387de0f77b6f91da37f1a69aa83cf715e
BigBoss435 <deividas.kalvelis@gmail.com>
2023-09-13 20:22:03 +0300

    ReverseNodesInKGroup LeetCode Challenge

--- MedianOfTwoSortedArrays/MedianOfTwoSortedArrays/Program.cs
+++ MedianOfTwoSortedArrays/MedianOfTwoSortedArrays/Program.cs
@@ -1,2 +1,76 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+public class Program
+{
+    /*Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+    The overall run time complexity should be O(log (m+n)).*/
+
+    public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+    {
+        if (nums1.Length > nums2.Length)
+        {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+
+        int m = nums1.Length;
+        int n = nums2.Length;
+        int imin = 0;
+        int imax = m;
+        int halfLen = (m + n + 1) / 2;
+
+        while (imin <= imax)
+        {
+            int i = (imin + imax) / 2;
+            int j = halfLen - i;
+
+            if (i < m && nums2[j - 1] > nums1[i])
+            {
+                imin = i + 1;
+            }
+            else if (i > 0 && nums1[i - 1] > nums2[j])
+            {
+                imax = i - 1;
+            }
+            else
+            {
+                int maxOfLeft;
+                if (i == 0)
+                {
+                    maxOfLeft = nums2[j - 1];
+                }
+                else if (j == 0)
+                {
+                    maxOfLeft = nums1[i - 1];
+                }
+                else
+                {
+                    maxOfLeft = Math.Max(nums1[i - 1], nums2[j - 1]);
+                }
+
+                if ((m + n) % 2 == 1)
+                {
+                    return maxOfLeft;
+                }
+
+                int minOfRight;
+                if (i == m)
+                {
+                    minOfRight = nums2[j];
+                }
+                else if (j == n)
+                {
+                    minOfRight = nums1[i];
+                }
+                else
+                {
+                    minOfRight = Math.Min(nums1[i], nums2[j]);
+                }
+
+                return (maxOfLeft + minOfRight) / 2.0;
+            }
+        }
+
+        return 0.0;
+    }
+}
---
+++ ReverseNodesInKGroup/ReverseNodesInKGroup/Program.cs
@@ -0,0 +1,56 @@
+﻿  public class ListNode 
+{
+      public int val;
+      public ListNode next;
+      public ListNode(int val=0, ListNode next=null) {
+          this.val = val;
+          this.next = next;
+      }
+  }
+ 
+
+public class Program
+{
+    public ListNode ReverseKGroup(ListNode head, int k)
+    {
+        {
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+
+            ListNode prevGroupTail = dummy;
+            ListNode curr = head;
+            int count = 0;
+
+            // Count the number of nodes in the linked list
+            while (curr != null)
+            {
+                count++;
+                curr = curr.next;
+            }
+
+            while (count >= k)
+            {
+                curr = prevGroupTail.next;
+                ListNode groupPrev = prevGroupTail;
+                ListNode groupNext = curr;
+
+                // Reverse k nodes in the current group
+                for (int i = 0; i < k; i++)
+                {
+                    ListNode nextTemp = curr.next;
+                    curr.next = groupPrev;
+                    groupPrev = curr;
+                    curr = nextTemp;
+                }
+
+                prevGroupTail.next = groupPrev;
+                groupNext.next = curr;
+
+                prevGroupTail = groupNext;
+                count -= k;
+            }
+
+            return dummy.next;
+        }
+    }
+}
