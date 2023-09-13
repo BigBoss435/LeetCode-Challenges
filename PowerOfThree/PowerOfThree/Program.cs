e2baffd132bddd2a89d829176c88594009b6284d
BigBoss435 <deividas.kalvelis@gmail.com>
2023-09-13 21:19:13 +0300

    PowerOfThree LeetCode Challenge

---
+++ PowerOfThree/PowerOfThree/Program.cs
@@ -0,0 +1,37 @@
+﻿
+/*Given an integer n, return true if it is a power of three. Otherwise, return false.
+An integer n is a power of three, if there exists an integer x such that n == 3x.*/
+
+public class Program
+{
+    public bool IsPowerOfThree(int n)
+    {
+        int division = n;
+        while (true)
+        {
+            if (n == 1 || n == 3)
+            {
+                return true;
+            }
+            else if (division % 3 != 0)
+            {
+                return false;
+            }
+            else if (division % 3 == 0)
+            {
+                while (division % 3 == 0)
+                {
+                    division /= 3;
+                    if (division == 3)
+                    {
+                        return true;
+                    }
+                    else if (division < 3)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+}
