f01b51bc81e450a95923b093748c72e26a0bec91
BigBoss435 <deividas.kalvelis@gmail.com>
2023-09-13 20:25:58 +0300

    Pow (x, n) LeetCode Challenge

---
+++ Pow (x, n)/Pow (x, n)/Program.cs
@@ -0,0 +1,13 @@
+﻿
+//Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+public class Program
+{
+    public class Solution
+    {
+        public double MyPow(double x, int n)
+        {
+            double power = Math.Pow(x, n);
+            return power;
+        }
+    }
+}
