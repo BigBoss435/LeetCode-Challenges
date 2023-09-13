7e2f6f45070c766872a75f36a24da50d53cb2204
BigBoss435 <deividas.kalvelis@gmail.com>
2023-09-13 21:20:47 +0300

    PowerOfFour LeetCode challenge

---
+++ PowerOfFour/PowerOfFour/Program.cs
@@ -0,0 +1,34 @@
+﻿
+public class Program
+{
+    public bool isPowerOfFour(int n)
+    {
+        int division = n;
+        while (true)
+        {
+            if (n == 1 || n == 4)
+            {
+                return true;
+            }
+            else if (division % 4 != 0)
+            {
+                return false;
+            }
+            else if (division % 4 == 0)
+            {
+                while (division % 4 == 0)
+                {
+                    division /= 4;
+                    if (division == 4)
+                    {
+                        return true;
+                    }
+                    else if (division < 4)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+}
